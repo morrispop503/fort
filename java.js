724bd41f6d70fbd008c74376bf77ce4f65c859f8
morrispop503 <136633072+morrispop503@users.noreply.github.com>
2023-06-15 16:52:28 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>1D Game</title>
+    <style>
+        #container {
+            width: 400px;
+            height: 200px;
+            position: relative;
+            border: 1px solid black;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+    <div id="container"></div>
+
+    <script src="script.js"></script>
+</body>
+</html>
